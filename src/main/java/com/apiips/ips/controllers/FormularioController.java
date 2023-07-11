28787b3b95ebe259a27be88618ca0c04182a0af3
Fabian Trujillo <facatrusa@gmail.com>
2023-07-10 19:11:45 -0500

    V 0.1.1

--- src/main/java/com/apiips/ips/controllers/FormularioController.java
+++ src/main/java/com/apiips/ips/controllers/FormularioController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
-@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/formulario")
 public class FormularioController {
