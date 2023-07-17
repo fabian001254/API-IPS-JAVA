3b558caf2866cfb06d6c94ac02eb3f468c1eeaf3
Fabian Trujillo <facatrusa@gmail.com>
2023-07-17 18:08:04 -0500

    Excepcion "ResponseStatusException" añadida V 0.1.3

--- src/main/java/com/apiips/ips/Errors/GlobalExceptionHandler.java
+++ src/main/java/com/apiips/ips/Errors/GlobalExceptionHandler.java
@@ -1,35 +1,41 @@
 package com.apiips.ips.Errors;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+        @ExceptionHandler(IllegalArgumentException.class)
+        public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+            ErrorResponse errorResponse = new ErrorResponse(ex.getMessage());
+            return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        }
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
-    public ErrorResponse handleIllegalArgumentException(IllegalArgumentException ex) {
-        return new ErrorResponse(ex.getMessage());
-    }
+        @ExceptionHandler(HttpMessageNotReadableException.class)
+        public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+            ErrorResponse errorResponse = new ErrorResponse("Error en la solicitud, revise los datos ingresados! " + ex.getMessage());
+            return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        }
 
-    @ExceptionHandler(HttpMessageNotReadableException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
-    public ErrorResponse handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
-        return new ErrorResponse("Error en la solicitud, revise los datos ingresados!, " + ex.getMessage());
-    }
-    
-    @ExceptionHandler(NullPointerException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
-    public ErrorResponse handleNullPointerException(NullPointerException ex) {
-        return new ErrorResponse(ex.getMessage());
-    }
+        @ExceptionHandler(NullPointerException.class)
+        public ResponseEntity<ErrorResponse> handleNullPointerException(NullPointerException ex) {
+            ErrorResponse errorResponse = new ErrorResponse(ex.getMessage());
+            return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        }
 
-}
+        @ExceptionHandler(ResponseStatusException.class)
+        public ResponseEntity<ErrorResponse> handleResponseStatusException(ResponseStatusException ex) {
+            ErrorResponse errorResponse = new ErrorResponse(ex.getReason());
+            return new ResponseEntity<>(errorResponse, ex.getStatusCode());
+        }
+
+
+   }
+
