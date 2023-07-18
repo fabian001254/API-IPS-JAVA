45de6e9536244027b6260a9f7ac9cdd45c2273bd
Fabian Trujillo <facatrusa@gmail.com>
2023-07-17 19:28:50 -0500

    Eliminacion de importaciones innecesarias

--- src/main/java/com/apiips/ips/Errors/GlobalExceptionHandler.java
+++ src/main/java/com/apiips/ips/Errors/GlobalExceptionHandler.java
@@ -5,8 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 
